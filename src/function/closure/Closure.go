601d6e094163f36e5b8662a5c6f2a931db86553b
2568808909 <2568808909@qq.com>
2020-05-25 16:10:15 +0800

    GO闭包

--- src/function/closure/Closure.go
+++ src/function/closure/Closure.go
@@ -16,6 +16,7 @@ func main() {
 	f := addUpdater()
 	//f其实就是一个闭包，f中包含着一个n和一个匿名函数，多次操作n的值会叠加
 	//这其实有点类似于面型对象的类，调用addUpdater返回的就是一个对象，n是一个字段,而匿名函数就是其方法
+	//n的值只会在addUpdater()函数时初始化一次，之后的运算都会对n进行叠加。如果重新调用一次addUpdater函数，将会返回一个新的闭包(类似于新的对象)。这个闭包的计算将与前一个无关
 	fmt.Println(f(1))
 	fmt.Println(f(2))
 	fmt.Println(f(3))
